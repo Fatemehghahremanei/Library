a13c7cedc5781834cde7622fa8e5e5addea83e82
Fatemehghahremanei <ghahremaneifatemeh@gmail.com>
2026-01-03 19:03:46 +0330

    تا اینجا درسته

--- src/app/+private/books-page/books-page.ts
+++ src/app/+private/books-page/books-page.ts
@@ -2,6 +2,7 @@ import { Component, inject, InjectionToken, OnInit } from '@angular/core';
 import { BooksService } from './books-service';
 import { FormsModule } from '@angular/forms';
 import { Thing } from '../../+shared/+base/base-thing';
+import { BaseCrudPage } from '../../+shared/+base/base-crud-page';
 
 @Component({
   selector: 'app-books-page',
@@ -9,51 +10,12 @@ import { Thing } from '../../+shared/+base/base-thing';
   templateUrl: './books-page.html',
   styleUrl: './books-page.scss',
 })
-export class BooksPage implements OnInit {
-  seve() {
-    if (this.state == 'add') {
-      this.booksService.add(this.item);
+export class BooksPage  extends BaseCrudPage<BookItem> implements OnInit {
 
-    }
-    else if (this.state == 'edit') {
-      this.booksService.edit(this.item);
-    }
-        else if (this.state == 'remove') {
-      this.booksService. remove(this.item);
-    }
-    this.dataRefresh();
-    this.state = 'list';
-  }
   ngOnInit(): void {
     this.dataRefresh();
   }
-  state: string = 'list';
-  data: BookItem[] = [];
-  item: BookItem = {
-    title: '',
-    writer: '',
-    publisher: ''
-  };
-  booksService = inject(BooksService);
-  dataRefresh() {
-    this.data = this.booksService.list();
-  }
-  add() {
-    this.state = 'add';
-    this.item = { title: '', writer: '', publisher: '' };
-  }
-  edit(book: BookItem) {
-    this.item = { ...book };
-    this.state = 'edit'
-  }
-  remove(book:BookItem){
-   this.item = { ...book };
-   this.state='remove';
-  }
-  cancel() {
-    this.state = 'list';
-  }
-
+  override setService = inject(BooksService);
 }
 export interface BookItem  extends Thing{
   title: string;
--- src/app/+private/member-page/member-page.ts
+++ src/app/+private/member-page/member-page.ts
@@ -2,6 +2,7 @@ import { Component, inject,InjectionToken, OnInit } from '@angular/core';
 import { MembersService } from './member-service';
 import { FormsModule } from '@angular/forms';
 import { Thing } from '../../+shared/+base/base-thing';
+import { BaseCrudPage } from '../../+shared/+base/base-crud-page';
 
 @Component({
   selector: 'app-members-page',
@@ -9,50 +10,12 @@ import { Thing } from '../../+shared/+base/base-thing';
   templateUrl: './Member-page.html',
   styleUrls: ['./Member-page.scss']
 })
-export class membersPage implements OnInit {
-  seve() {
-    if (this.state == 'add') {
-      this.membersService.add(this.item);
-
-    }
-    else if (this.state == 'edit') {
-      this.membersService.edit(this.item);
-    }
-    else if (this.state == 'remove') {
-      this.membersService.remove(this.item);
-    }
-    this.dataRefresh();
-    this.state = 'list';
-  }
+export class membersPage extends BaseCrudPage<memberItem> implements OnInit {
   ngOnInit(): void {
     this.dataRefresh();
   }
-   state: string = 'list';
-  data: memberItem[] = [];
-  item: memberItem = {
-    firstname: '',
-    lastname: '',
-  };
-  membersService = inject(MembersService)
-  dataRefresh() {
-    this.data = this.membersService.list();
+  override setService=inject(MembersService);
   }
-  add() {
-    this.state = 'add';
-     this.item = { firstname: '', lastname: '' };
-  }
-    edit(members: memberItem) {
-      this.item = { ...members};
-      this.state = 'edit'
-    }
-    remove(members:memberItem){
-     this.item = { ...members};
-     this.state='remove';
-    }
-  cancel() {
-    this.state = 'list';
-  }
-}
 export interface memberItem extends Thing{
   firstname: string;
   lastname: string;
